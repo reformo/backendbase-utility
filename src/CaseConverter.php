2dcb09350e05c7b799b07f95d81fc215bf772791
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-07-14 00:01:52 +0300

    separator added for CaseCoverter

--- src/CaseConverter.php
+++ src/CaseConverter.php
@@ -9,13 +9,14 @@ class CaseConverter
      * Returns PascalCase string
      *
      * @param string $source
+     * @param string $separator
      * @return string
      *
      */
-    public static function toPascalCase(string $source) : string
+    public static function toPascalCase(string $source, string $separator='_') : string
     {
         // If the string is snake case
-        $modified = str_replace('_', ' ', $source);
+        $modified = str_replace($separator, ' ', $source);
         $lowercase = mb_strtolower($modified);
         $uppercaseFirstLetters = mb_convert_case($lowercase, MB_CASE_TITLE);
         return str_replace(' ', '', $uppercaseFirstLetters);
@@ -25,26 +26,28 @@ class CaseConverter
      * Returns camelCase string
      *
      * @param string $source
+     * @param string $separator
      * @return string
      *
      */
-    public static function toCamelCase(string $source) : string
+    public static function toCamelCase(string $source, string $separator='_') : string
     {
-        return lcfirst(self::toPascalCase($source));
+        return lcfirst(self::toPascalCase($source, $separator));
     }
 
     /**
      * Returns snake_case string
      *
      * @param string $source
+     * @param string $separator
      * @return string
      *
      */
-    public static function toSnakeCase(string $source) : string
+    public static function toSnakeCase(string $source, string $separator='_') : string
     {
         // If the string is pascal/camel case
         $modified = str_replace('  ',' ',preg_replace('/[A-Z]+/', ' $0', $source));
         $lowercase = mb_strtolower(trim($modified));
-        return str_replace(' ', '_', $lowercase);
+        return str_replace(' ', $separator, $lowercase);
     }
 }
